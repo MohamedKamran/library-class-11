ef99e23c82695984d55cf3a9522d0eaf55ddf018
MohamedKamran <96822969+MohamedKamran@users.noreply.github.com>
2021-12-30 13:08:17 +0530

    Updated backend.py

--- backend.py
+++ backend.py
@@ -3,24 +3,40 @@
 
 library=[]
 
-from datetime import datetime
+from datetime import datetime,timedelta
 
 today = datetime.now()
 
-def cdate(date):
-  datei = [date.year,date.month,date.day]
-  return(datei)
-today = cdate(today)
 
-def entry():
+def display(set,flag): # helps to portrait the results
+  #book=[id,name,author,cateogory,arrival[borrower,cdate,rdate,fine]]
+  if flag == False:
+    print("id\tname\tauthor\tcateogory\tarrival")
+    for book in set:
+      for det in book:
+        print(det,end="\t")
+      print()
+  elif flag == True:
+    print("id\tname\tauthor\tcateogory\tarrival\tborrow\tDate\tReturn\tFine")
+    for book in set:
+      for det in book:
+        if type(det)==list:
+          for i in det:
+            print(i,end="\t")
+        else:
+          print(det,end="\t")
+      print()
+
+def entry(): # Automated Entry System
   name = input("Enter book name:")
   auth = input("Author Name:")
   cat=input("Enter which caateogory:")
   id=(input("Enter New id:"))
   adate=today
-  book=[name,auth,cat,id,adate,[False,False,False,False]]
+  book=[id,name,auth,cat,adate,[False,False,False,False]]
   library.append(book)
-  print("Added",str(len(library)+1)+"th book",library[-1][1])
+  print("Added",str(len(library))+"th book",library[-1][1])
+  #print(library)
 
 def search(filter,type):
   req=[]
@@ -38,7 +54,8 @@ def p_search():
   print("Select by which you want to filter \n1 for id\n2 for name\n3 for author \n4for cateogory")
   t1 = int(input("Enter the field:"))-1
   f1 = input("Enter search: ")
-  search(f1,t1)
+  r=search(f1,t1)
+  display(r[1],True)
   
 def change_com(id_r,flag): # flag => 1(new)
   room = search(id_r,0)
@@ -48,27 +65,28 @@ def change_com(id_r,flag): # flag => 1(new)
   if flag == 1:
     book_r[5][0]=input("Borrower Name")
     book_r[5][1]=today
-    delt = datetime.delta(days=15)
+    delt = timedelta(days=15)
     book_r[5][2]=today+delt
     print("{} has borrowed {} should return at {}".format(book_r[5][0],book_r[1],book_r[5][2]))
   else:
     det = book_r[5]
-    print("{} has returned {} at {} and payed{}".format(book_r[5][0],book_r[1],today,flag)
+    print("{} has returned {} at {} and payed{}".format(book_r[5][0],book_r[1],today,flag))
     for x in det:
       x=False 
 
 def check(flag):
   fault = []
   for m in library:
-  date = m[5][2]
-  if date>today:
-    fine = m[5][3]
-    fine = today-date
-    fine=fine.days*5
-    fault.append(m)
+    date = m[5][2]
+    print(today,date)
+    if date>today:
+      fine = m[5][3]
+      fine = today-date
+      fine=int(fine.days)*5
+      fault.append(m)
   if flag == True:
     for wr in fault:
-      print(wr[1],"borrowed by",wr[5][0],"and has to pay",str(50+fine)
+      print(wr[1],"borrowed by",wr[5][0],"and has to pay",str(50+fine))
 
 def checkout(id_pay):
   check(False) # Update Fine
@@ -77,15 +95,22 @@ def checkout(id_pay):
   fine=fine.days*5
   change_com(id_pay,fine)
   print("Thank you for borrowing books from us. Hope you visit us next time.")
-
-def display(set,flag):
-  #book=[id,name,author,cateogory,arrival[borrower,cdate,rdate,fine]]
-  if flag == False:
-    print("id\tname\tauthor\tcateogory\tarrival")
-    for book in set:
-      for det in book:
-        if type(det)==list:
-          print(det,end="\t")
-          break
-  elif flag == True:
-    
+menu="1 for Entering Books \n2 for search \n3 for cashier system \n4 for Checking Fine \n5 for Bill display \n6 for end"
+while True:
+  print(menu)
+  op = int(input("Enter choice"))
+  if op == 1:
+    entry()
+  elif op == 2:
+    p_search()
+  elif op == 3:
+    me = input("Enter id:")
+    change_com(me,1)
+  elif op == 4:
+    check(True)
+  elif op == 5:
+    mep = input("Enter id:")
+    change_com(mep)
+  elif op == 6:
+    break
+  print("------------------------------------")
