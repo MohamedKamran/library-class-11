7b17cbae1413fae12b80b30a5877b54d1346d2b0
MohamedKamran <96822969+MohamedKamran@users.noreply.github.com>
2022-01-01 23:52:52 +0530

    Added new feature ratings
    Customers are now able to rate the books when purchased

--- backend.py
+++ backend.py
@@ -1,20 +1,45 @@
-# library = [...[id,name,author,cateogory,arrival[borrower,cdate,rdate,fine]]...]
+# library = [...[id,name,author,cateogory,arrival[borrower,cdate,rdate,fine],rating]...]
 # Default = 50 | Fine = 5 Per Day
+# New : Added Ratings
 
 library=[]
 
-from datetime import datetime,timedelta
+from datetime import date as datetime
+from datetime import timedelta
 
-today = datetime.now()
+today = datetime.today()
 
-
+def sortByDate(lib):
+    n = len(lib)
+    for i in range(n):
+      for j in range(0, n-i-1):
+        if lib[j][5][2] > lib[j+1][5][2] :
+          lib[j], lib[j+1] = lib[j+1], lib[j]
+              
+def sortByRatings(lib):
+  n = len(lib)
+  for i in range(n):
+    for j in range(0, n-i-1):
+      a1,a2=lib[j][6],lib[j+1][6]
+      a1,a2=sum(a1)/len(a1),sum(a2)/len(a2)
+      if a1 > a2 :
+        lib[j], lib[j+1] = lib[j+1], lib[j]
+              
+#ToDo: Fix display (Done @ 80%)
 def display(set,flag): # helps to portrait the results
   #book=[id,name,author,cateogory,arrival[borrower,cdate,rdate,fine]]
   if flag == False:
-    print("id\tname\tauthor\tcateogory\tarrival")
+    print("id\tname\tauthor\tcateogory\tarrival\tRatings")
+    i = 0
     for book in set:
+      semic=book[-1]
+      semic=sum(semic)/len(semic)
       for det in book:
         print(det,end="\t")
+        i+=1
+        if i==6:
+          break
+        print(semic)
       print()
   elif flag == True:
     print("id\tname\tauthor\tcateogory\tarrival\tborrow\tDate\tReturn\tFine")
@@ -32,22 +57,28 @@ def entry(): # Automated Entry System
   auth = input("Author Name:")
   cat=input("Enter which caateogory:")
   id=(input("Enter New id:"))
+  for bk in library:
+    if bk[0]==id:
+      print("Sorry, but the id is already entered")
+      id=(input("Enter New id:"))
   adate=today
-  book=[id,name,auth,cat,adate,[False,False,False,False]]
+  ratings=[] # fix rating system
+  book=[id,name,auth,cat,adate,[False,False,False,False],ratings]
   library.append(book)
   print("Added",str(len(library))+"th book",library[-1][1])
   #print(library)
 
 def search(filter,type):
-  req=[]
-  req_r=[]
+  req=[]#to be sold
+  req_s=[] #already sold
   for book in library:
     if book[type]==filter:
-      if book[5][0] is not False:
+      if book[5][0] is False:
         req.append(book)
       else:
-        req_r.append(book)
-  sam = [req,req_r]
+        req_s.append(book)
+  sortByDate(req_s)
+  sam = [req,req_s] # 0 unsold ; 1 sold
   return sam
 
 def p_search():
@@ -55,15 +86,17 @@ def p_search():
   t1 = int(input("Enter the field:"))-1
   f1 = input("Enter search: ")
   r=search(f1,t1)
+  display(r[0],False) # ToDo: Fix display
+  print();print("Books upto come: ")
   display(r[1],True)
   
-def change_com(id_r,flag): # flag => 1(new)
+def change_com(id_r,flag,stat): # stat: 0 unsold ; 1 sold
   room = search(id_r,0)
-  room = room[1]
+  room = room[stat]
   for books in room:
     book_r = books
   if flag == 1:
-    book_r[5][0]=input("Borrower Name")
+    book_r[5][0]=input("Borrower Name:")
     book_r[5][1]=today
     delt = timedelta(days=15)
     book_r[5][2]=today+delt
@@ -91,11 +124,30 @@ def check(flag):
 
 def checkout(id_pay):
   check(False) # Update Fine
-  fine = m[5][3]
+  m=search(id_pay,0)
+  for i in m:
+    pass
+  for j in i:
+    print(j)
+  date = j[5][2]
+  #print(fine)
   fine = today-date
   fine=fine.days*5
-  change_com(id_pay,fine)
+  j[5][3]=fine
+  change_com(id_pay,fine,1)
+  rater=int(input("How much would you rate this book for 5:"))
+  while True:
+    if rater<=5:
+      print("Thank you for Feedback")
+      break
+    else:
+      print("Sorry, Enter value than 5")
+      rater=int(input("How much would you rate this book for 5"))
+    rate_fi=m[6]
+    rate_fi.append()
   print("Thank you for borrowing books from us. Hope you visit us next time.")
+
+#--------------------------------------------------------------------------------------------------------
 menu="1 for Entering Books \n2 for search \n3 for cashier system \n4 for Checking Fine \n5 for Bill display \n6 for end"
 while True:
   print(menu)
@@ -106,12 +158,12 @@ while True:
     p_search()
   elif op == 3:
     me = input("Enter id:")
-    change_com(me,1)
+    change_com(me,1,0)
   elif op == 4:
     check(True)
   elif op == 5:
     mep = input("Enter id:")
-    change_com(mep)
+    checkout(mep)
   elif op == 6:
     break
   """
@@ -119,18 +171,18 @@ while True:
   Code 198 is made for Develepors for testing the Software
   and demo Please disable this feature for production
   """
-  elif op == 198:
-    print("Warning: Entering into Developer Mode.\n This is to change current date\n This is made only for Testing.")
-    p = []
-    qus = ("Year","Month","Day")
-    for q in qus:
-      query = ""
-      defi = "Enter New",q,":"
-      for de in defi:
-        query+=de+" "
-      epo = int(input(query))
-      p.append(epo)
-    # Creating Future today
-    today = datetime(p[0],p[1],p[2])
-    print("Entering Future. Assume that day is",today)
-  print("------------------------------------")
+  # elif op == 198:
+  #   print("Warning: Entering into Developer Mode.\n This is to change current date\n This is made only for Testing.")
+  #   p = []
+  #   qus = ("Year","Month","Day")
+  #   for q in qus:
+  #     query = ""
+  #     defi = "Enter New",q,":"
+  #     for de in defi:
+  #       query+=de+" "
+  #     epo = int(input(query))
+  #     p.append(epo)
+  #   # Creating Future today
+  #   today = datetime(p[0],p[1],p[2])
+  #   print("Entering Future. Assume that day is",today)
+  # print("------------------------------------")
